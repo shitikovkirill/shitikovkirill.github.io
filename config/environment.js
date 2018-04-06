d260ca0d6b63f758f27d39dfe981123070557833
Shitikov Kirill <sh.kiruh@gmail.com>
2018-04-06 09:51:53 +0300

    Change root url

--- config/environment.js
+++ config/environment.js
@@ -45,7 +45,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/shitikovkirill-github-io/';
+    ENV.rootURL = '/';
     // here you can enable a production-specific feature
   }
 

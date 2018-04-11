770099c9d46c7bb6956001f4ebd3fc56939b56de
Shitikov <sh.kiruh@gmail.com>
2018-04-11 18:03:07 +0300

    Draw Wall and Pallet

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -7,20 +7,42 @@ export default Component.extend(KeyboardShortcuts, {
   x: 1,
   y: 2,
   squareSize: 40,
-  screenWidth: 20,
-  screenHeight: 15,
+  grid: [
+    [2,2,2,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,2,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,2,2,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,2,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [1,2,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+  ],
   ctx: computed(function () {
     let canvas = document.getElementById("myCanvas");
     let ctx = canvas.getContext("2d");
     return ctx;
   }),
-  screenPixelWidht: computed(function () {
+  screenWidth: computed(function () {
+    return this.get('grid.firstObject.length');
+  }),
+  screenHeight: computed(function () {
+    return this.get('grid.length');
+  }),
+  screenPixelWidth: computed(function () {
     return this.get('screenWidth') * this.get('squareSize');
   }),
   screenPixelHeight: computed(function () {
     return this.get('screenHeight') * this.get('squareSize');
   }),
   didInsertElement: function () {
+    this.drawWalls();
     this.drawCircle();
   },
   drawCircle: function () {
@@ -38,6 +60,36 @@ export default Component.extend(KeyboardShortcuts, {
     ctx.closePath();
     ctx.fill();
   },
+  drawWalls: function(){
+    let squareSize = this.get('squareSize');
+    let ctx = this.get('ctx');
+    ctx.fillStyle = '#000';
+
+    let grid = this.get('grid');
+    grid.forEach(function (row, rowIndex) {
+      row.forEach(function (cell, columnIndex) {
+          if(cell === 1){
+            ctx.fillRect(
+              columnIndex * squareSize,
+              rowIndex * squareSize,
+              squareSize,
+              squareSize
+            )
+          }
+
+          if(cell === 2){
+            let pixelX = (columnIndex + 1 / 2) * squareSize;
+            let pixelY = (rowIndex + 1 / 2) * squareSize;
+
+            ctx.beginPath();
+            ctx.arc(pixelX, pixelY, squareSize / 6, 0, Math.PI * 2, false);
+            ctx.closePath();
+            ctx.fill();
+          }
+        }
+      );
+    })
+  },
   clearScreen: function () {
     let ctx = this.get('ctx');
     ctx.clearRect(0, 0, this.get('screenPixelWidth'), this.get('screenPixelHeight'));
@@ -45,12 +97,13 @@ export default Component.extend(KeyboardShortcuts, {
   movePacMan: function (direction, amount) {
     this.incrementProperty(direction, amount);
 
-    if(this.collidedWithBorder()){
+    if(this.collidedWithBorder() || this.collidedWithWall()){
       this.decrementProperty(direction, amount);
     }
 
     this.clearScreen();
     this.drawCircle();
+    this.drawWalls();
   },
   keyboardShortcuts: {
     up: function () {
@@ -79,5 +132,12 @@ export default Component.extend(KeyboardShortcuts, {
     let pacOutOfBounds = x < 0 || y < 0 || x >= screenWidth || y >= screenHeight;
 
     return pacOutOfBounds;
+  },
+  collidedWithWall: function () {
+    let x = this.get('x');
+    let y = this.get('y');
+    let grid = this.get('grid');
+
+    return grid[y][x] == 1
   }
 });

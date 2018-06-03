03e2bbe09b9b85f933c8399ae5d595b0ff5893ef
Shitikov Kirill <sh.kiruh@gmail.com>
2018-06-03 14:56:41 +0300

    Displaying a List of Items

--- app/routes/tutorial/routes/will-transition.js
+++ app/routes/tutorial/routes/will-transition.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
+import Route from '@ember/routing/route';
+import { later } from '@ember/runloop';
 
-export default Ember.Route.extend ({
+export default Route.extend ({
   actions: {
     willTransition(transition) {
 
@@ -15,7 +17,7 @@ export default Ember.Route.extend ({
         Ember.Logger.log("transition abort");
         transition.abort();  //calling abort function
 
-        Ember.run.later(function () {
+        later(function () {
           //setting the self variable to true
           if(result){
             self.set('allowTransition', true);
